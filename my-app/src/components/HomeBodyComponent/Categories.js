38ee63891d20752a1cb972d06184aef4786d1ad3
T-DucDung <cbcbvn99@mail.com>
2020-09-10 10:18:05 +0700

    chuan bi push

--- my-app/src/components/HomeBodyComponent/Categories.js
+++ my-app/src/components/HomeBodyComponent/Categories.js
@@ -1,30 +1,63 @@
-import React from 'react';
-import SliderItem from './SliderItem'
+import React from "react";
 import ReactDOM from "react-dom";
-import Carousel from "react-elastic-carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import SliderItem from "./SliderItem"
 
-const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 }
-  ];
+import Slider from "react-slick";
 
 class Categories extends React.Component {
     render() {
+        var settings = {
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            nextArrow: false,
+            prevArrow: false,
+            swipeToSlide: true,
+            initialSlide: 0,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        infinite: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                    }
+                }
+            ]
+        };
         return (
-            <div className="App">
-                <div className="container">
-                <Carousel breakPoints={breakPoints}>
-                    <SliderItem></SliderItem>
-                    <SliderItem></SliderItem>
-                    <SliderItem></SliderItem>
-                    <SliderItem></SliderItem>
-                    <SliderItem></SliderItem>
-                    <SliderItem></SliderItem>
-                    <SliderItem></SliderItem>
-                    <SliderItem></SliderItem>
-                </Carousel>
+            <div className="container">
+                <div className="App">
+                    <Slider {...settings}>
+                        <SliderItem></SliderItem>
+                        <SliderItem></SliderItem>
+                        <SliderItem></SliderItem>
+                        <SliderItem></SliderItem>
+                        <SliderItem></SliderItem>
+                        <SliderItem></SliderItem>
+                        <SliderItem></SliderItem>
+                        <SliderItem></SliderItem>
+                    </Slider>
                 </div>
             </div>
         );
--- my-app/src/components/HomeBodyComponent/SliderItem.js
+++ my-app/src/components/HomeBodyComponent/SliderItem.js
@@ -3,10 +3,8 @@ import React from 'react';
 class SliderItem extends React.Component {
     render() {
         return (
-            <div class="col-lg-3">
-                <div className="categories__item" style={{backgroundImage: `url(${process.env.PUBLIC_URL + 'img/categories/cat-1.jpg'}`, width: '270px' }}>
-                    <h5><a href="#">Fresh Fruit</a></h5>
-                </div>
+            <div className="categories__item" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + 'img/categories/cat-1.jpg'}`, width: '270px', margin: "auto"}}>
+                <h5><a href="#">Fresh Fruit</a></h5>
             </div>
         );
     }
